08d8c8981e022c6fa94f4c1bc3fd8f0e518c1468
C1ayemore <isip_a.a.bokov@mpt.ru>
2022-10-31 23:56:17 +0300

    сделал

--- Program.cs
+++ Program.cs
@@ -1,71 +1,145 @@
-﻿using NotePad__;
+﻿
+using shirlitc;
 
-static void Menu(DateTime date) //вывод мейн менюшки
+namespace shirlitc
 {
-    Console.WriteLine($"{date.ToString("D")}");
-    Console.WriteLine("   1. Посмотреть все заметки на этот день");
-    Console.WriteLine("   2. Создать заметку на этот день");
-    Console.WriteLine("   3. Выйти из программы");
-}
-
-static void ReadNotes(DateTime date, IEnumerable<Note> DayNotes)
-{
-    Console.Clear();
-    Console.WriteLine($"Дата заметок - {date.ToString("D")}");
-    foreach (Note note in DayNotes)
+    internal class Заметки
     {
-        Console.WriteLine($"   {note.name}");
+        public DateTime date;
+        public string content;
+        public string title;
+        public int position;
     }
-    Console.WriteLine("   ");
-    Console.WriteLine("   Выход в меню");
 }
 
-static void AbouteNote(DateTime date, Note note)
-{
-    Console.Clear();
-    Console.WriteLine($"{date.ToString("D")}");
-    Console.WriteLine("=========================================");
-    Console.WriteLine($"{note.name}"); //Вывод названия заметки
-    Console.WriteLine($"{note.text}"); //Вывод содержимого заметки
-    Console.WriteLine("=========================================");
-    Console.WriteLine("Нажмите любую клавишу, чтобы выйти из этого меню");
-    Console.ReadKey();
-}
 
-static void AppendNotes(DateTime date, List<Note> notes) //создание заметок
+internal class Program
 {
-    Console.Clear();
-    Console.WriteLine("Введите название заметки:");
-    string name = Console.ReadLine();
-    Console.WriteLine("Введите основной текст заметки:");
-    string text = Console.ReadLine();
-    notes.Add(new Note() { date = date, name = name, text = text });
-}
-
-static int UpdateCursorPos(int VerPos, int MaxVerPos, int MinVerPos, ConsoleKey key) //Изменение позиции курсора
-{
-    switch (key)
+    static void Main()
     {
-        case ConsoleKey.W:
-            VerPos--;
-            if (VerPos < MinVerPos)
+        List<Заметки> notes = new List<Заметки>();
+
+        Заметки note = new Заметки();
+
+        DateTime dateTime = DateTime.Today;
+
+        note.title = "прятки";
+        note.content = "В 8.00";
+        note.date = dateTime;
+        note.position = 1;
+        notes.Add(note);
+
+        Заметки note2 = new Заметки();
+
+        note2.title = "Пойти сабаку";
+        note2.content = "пред обедом";
+        note2.date = dateTime.AddDays(+1);
+        note2.position = 2;
+        notes.Add(note2);
+
+        Заметки note3 = new Заметки();
+
+        note3.title = "Зайти к михаилу";
+        note3.content = "перед парами";
+        note3.date = dateTime.AddDays(+1);
+        note3.position = 3;
+        notes.Add(note3);
+
+        Заметки note4 = new Заметки();
+
+        note4.title = "поцибарить";
+        note4.content = "на ахен";
+        note4.date = dateTime.AddDays(+3);
+        note4.position = 4;
+        notes.Add(note4);
+
+        Заметки note5 = new Заметки();
+
+        note5.title = "Штирлиц в 1996";
+        note5.content = "в 18:00";
+        note5.date = dateTime.AddDays(+3);
+        note5.position = 3;
+        notes.Add(note5);
+
+        Заметки note6= new Заметки();
+
+        note6.title = "За гаражами все дела";
+        note6.content = " На рыбалке";
+        note6.date = dateTime.AddDays(-3);
+        note6.position = 3;
+        notes.Add(note6);
+
+        DateTime date = DateTime.Now;
+        int position = 1;
+
+        while (true)
+        {
+            ConsoleKeyInfo Key = Console.ReadKey();
+            if (Key.Key == ConsoleKey.RightArrow)
             {
-                VerPos = MinVerPos;
+                date = date.AddDays(1);
             }
-            break;
-        case ConsoleKey.S:
-            VerPos++;
-            if (VerPos > MaxVerPos)
+            else if (Key.Key == ConsoleKey.LeftArrow)
             {
-                VerPos = MaxVerPos;
-            };
-            break;
-    }
-    return VerPos;
-}
+                date = date.AddDays(-1);
+            }
+            Console.Clear();
+            Console.WriteLine(date.ToString("dd.MM.yyyy"));
+            foreach (Заметки zam in notes)
+            {
+                if (zam.date.Date == date.Date)
+                {
+                    Console.WriteLine("  " + zam.title);
+                }
+            }
+            if (Key.Key == ConsoleKey.UpArrow)
+            {
+                position--;
+            }
+            else if (Key.Key == ConsoleKey.DownArrow)
+            {
+                position++;
+            }
+            if (position < 2)
+            {
+                position = 1;
+            }
+            else if (position > 2)
+            {
+                position = 1;
+            }
+            Console.SetCursorPosition(0, position);
+            Console.WriteLine("->");
 
-static void WriteCursor(int VerPos) //создание курсора в позиции
-{
-    Console.SetCursorPosition(0, VerPos);
-    Console.WriteLine("-->");
+            if (Key.Key == ConsoleKey.Enter)
+            {
+                foreach (Заметки pos in notes)
+                {
+                    if (note.position == position)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Информация о заметке: ");
+                        Console.WriteLine("Название: ");
+                        Console.WriteLine(note.title);
+                        Console.WriteLine("Содержание: ");
+                        Console.WriteLine(note.content);
+                    }
+                    if (note2.position == position)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Информация о заметке: ");
+                        Console.WriteLine("Название: ");
+                        Console.WriteLine(note.title);
+                        Console.WriteLine("Содержание: ");
+                        Console.WriteLine(note.content);
+                    }
+                    if (note3.position == position)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Информация о заметке");
+                    }
+                }
+            }
+        }
+    }
 }
